5ac4e3d19ce4365e5c5902eb5790ff492e3d82d4
Tomokazu-Matsui <ruyuejiecheng@gmail.com>
2023-06-07 15:30:08 +0900

    商品出品機能修正３回目

--- app/models/item.rb
+++ app/models/item.rb
@@ -29,5 +29,5 @@ class Item < ApplicationRecord
     validates :item_scheduled_delivery_id
   end
 
-  validates :item_price, numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999 }
+  validates :item_price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
